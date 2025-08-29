f6d04635ec07872128ca75deb0ffec3bf86c9378
SaidSamit <s.samit@alumnos.santotomas.cl>
2025-08-29 18:32:10 -0400

    Funcionalidad activities viernes y sabado

--- app/src/main/java/com/unidad/gymapp/FridayActivity.java
+++ app/src/main/java/com/unidad/gymapp/FridayActivity.java
@@ -1,12 +1,26 @@
 package com.unidad.gymapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FridayActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monday);
+        setContentView(R.layout.activity_friday);
+
+        Button backBtn = findViewById(R.id.button2);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FridayActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
